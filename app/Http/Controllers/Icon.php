8df120f20883b0e2ab100dcb6a1904f33bcf618c
Yassin <yassin.ntumba@gmail.com>
2021-06-04 14:27:14 +0200

    commit

---
+++ app/Http/Controllers/Icon.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\icons;
+use Illuminate\Http\Request;
+
+class IconsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\icons  $icons
+     * @return \Illuminate\Http\Response
+     */
+    public function show(icons $icons)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\icons  $icons
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(icons $icons)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\icons  $icons
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, icons $icons)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\icons  $icons
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(icons $icons)
+    {
+        //
+    }
+}
---
+++ database/factories/IconsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\icons;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class IconsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = icons::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
